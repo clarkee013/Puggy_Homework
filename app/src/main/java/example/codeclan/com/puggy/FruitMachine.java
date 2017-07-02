1d9b4ee579ae2d3c66447507cc61fcb83f83fabd
David Clarkson <clarkee013@gmail.com>
2017-07-02 11:47:19 +0100

    infinite loop in getAllSpinResults in Fruitmachine class, mockito testing required in fruitmachine test - commit before going down the route of changing wheel barrell to wheel + 3xwheel

--- app/src/main/java/example/codeclan/com/puggy/FruitMachine.java
+++ app/src/main/java/example/codeclan/com/puggy/FruitMachine.java
@@ -13,27 +13,40 @@ public class FruitMachine {
     Wheel barrel1;
     Wheel barrel2;
     Wheel barrel3;
-    ArrayList<Symbol>spinResult;
+    ArrayList<Symbol>spinResults;
+    ArrayList<Symbol>barrel;
+    int randomlySelectedIndex;
 
 
-    public FruitMachine(){
+    public FruitMachine() throws Exception {
         this.player = new Player(5);
         this.barrel1 = new Wheel();
         this.barrel2 = new Wheel();
         this.barrel3 = new Wheel();
         spinWheels();
+
+
     }
 
     public void spinWheels(){
         this.barrel1.spin();
         this.barrel2.spin();
         this.barrel3.spin();
-
     }
 
+//    public ArrayList<Symbol> allSpinResults() throws Exception{
+//        spinResults.add(barrel1.getSpinResult());
+//        spinResults.add(barrel2.getSpinResult());
+//        spinResults.add(barrel3.getSpinResult());
+//        return allSpinResults();
+//    }
 
-
-
+    public ArrayList<Symbol> allSpinResults() throws Exception{
+        allSpinResults().add(barrel1.getSpinResult());
+        allSpinResults().add(barrel2.getSpinResult());
+        allSpinResults().add(barrel3.getSpinResult());
+        return allSpinResults();
+    }
 
 
 }
--- app/src/main/java/example/codeclan/com/puggy/Runner.java
+++ app/src/main/java/example/codeclan/com/puggy/Runner.java
@@ -1,11 +1,19 @@
 package example.codeclan.com.puggy;
 
+import android.app.FragmentTransaction;
+
 /**
  * Created by user on 30/06/2017.
  */
 
 public class Runner {
 
+    public static void main(String[] args) {
+
+
+
+    }
 
 
 }
+
